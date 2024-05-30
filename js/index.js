a8816fe925020c1d0cc083a83a8e2b5a6fa0331d
Igor_Polyakov <itworldigpo@gmail.com>
2024-05-30 17:48:27 +0300

    ads

--- js/index.js
+++ js/index.js
@@ -16,51 +16,53 @@ burgerMenu.addEventListener('click', function(){
 
 // PARALAX================================================
 
-
-window.onload = function(){
-    const paralax = document.querySelector ('.basic');
-
-    if (paralax && window.innerWidth >=768){ 
-        const pasta = document.querySelector ('.pasta-image');
-        const saladOne = document.querySelector ('.basic__image-sald1');
-        const saladTwo = document.querySelector ('.basic__image-sald2');
-
-
-        const forPasta = 5;
-        const forsaladOne = 3;
-        const forsaladTwo = 4;
-
-        const speed = 0.05;
-
-
-        let positionX = 0 , positionY = 0;
-        let coordXprocent = 0 , coordYprocent = 0;
-
-        function setMouthParalaxStyle (){
-            const distX = coordXprocent - positionX;
-            const distY = coordYprocent - positionY;
-            positionX = positionX + (distX * speed);
-            positionY = positionY + (distY * speed);
-
-            pasta.style.cssText = `transform: translate(${positionX / forPasta}%, ${positionY / forPasta}%)`;
-            saladOne.style.cssText = `transform: translate(${positionX / forsaladOne}%, ${positionY / forsaladOne}%)`;
-            saladTwo.style.cssText = `transform: translate(${positionX / forsaladTwo}%, ${positionY / forsaladTwo}%)`;
-
-            requestAnimationFrame(setMouthParalaxStyle);
+if (window.innerWidth >=768){
+    window.onload = function(){
+        const paralax = document.querySelector ('.basic');
+    
+        if (paralax){ 
+            const pasta = document.querySelector ('.pasta-image');
+            const saladOne = document.querySelector ('.basic__image-sald1');
+            const saladTwo = document.querySelector ('.basic__image-sald2');
+    
+    
+            const forPasta = 5;
+            const forsaladOne = 3;
+            const forsaladTwo = 4;
+    
+            const speed = 0.05;
+    
+    
+            let positionX = 0 , positionY = 0;
+            let coordXprocent = 0 , coordYprocent = 0;
+    
+            function setMouthParalaxStyle (){
+                const distX = coordXprocent - positionX;
+                const distY = coordYprocent - positionY;
+                positionX = positionX + (distX * speed);
+                positionY = positionY + (distY * speed);
+    
+                pasta.style.cssText = `transform: translate(${positionX / forPasta}%, ${positionY / forPasta}%)`;
+                saladOne.style.cssText = `transform: translate(${positionX / forsaladOne}%, ${positionY / forsaladOne}%)`;
+                saladTwo.style.cssText = `transform: translate(${positionX / forsaladTwo}%, ${positionY / forsaladTwo}%)`;
+    
+                requestAnimationFrame(setMouthParalaxStyle);
+            }
+            setMouthParalaxStyle();
+    
+            paralax.addEventListener('mousemove', function(e){
+                const paralaxWidth = paralax.offsetWidth;
+                const paralaxHeight = paralax.offsetHeight;
+    
+                const coordX = e.pageX - paralaxWidth / 2;
+                const coordY = e.pageY - paralaxHeight / 2;
+    
+                coordXprocent = coordX / paralaxWidth * 100;
+                coordYprocent = coordY / paralaxHeight * 100;
+    
+            })
         }
-        setMouthParalaxStyle();
-
-        paralax.addEventListener('mousemove', function(e){
-            const paralaxWidth = paralax.offsetWidth;
-            const paralaxHeight = paralax.offsetHeight;
-
-            const coordX = e.pageX - paralaxWidth / 2;
-            const coordY = e.pageY - paralaxHeight / 2;
-
-            coordXprocent = coordX / paralaxWidth * 100;
-            coordYprocent = coordY / paralaxHeight * 100;
-
-        })
     }
 }
+
 // ===========================================================================
